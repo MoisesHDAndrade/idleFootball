4ff58c88ed5e1759e4315a97422ab201181e9a88
Moises <lawliex@gmail.com>
2020-08-28 01:44:31 +1200

    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class App:
 		
 		self.cup = []
 		self.groups = []
-		self.round_oc = []
+		self.round_of_sixteen = []
 
 	def define_cup(self):
 		with open('src/teams.txt','r') as file:
@@ -35,7 +35,7 @@ class App:
 			
 			contador+=1
 
-	def team_matches(self):
+	def team_matches_groups(self):
 		lista = []
 		results = []
 		for item in self.groups:
@@ -47,12 +47,12 @@ class App:
 			results.append(f"{item[0]['team']} {item[0]['goals']} X {item[1]['goals']} {item[1]['team']}")
 
 			if item[0]['goals'] > item[1]['goals']:
-				item[0]['points']+=3
-				item[0]['victory']+=1
-				item[0]['total_goals'] += item[0]['goals']
-				item[0]['goals_taken'] += item[1]['goals']
+				item[0]['points'] += 3 #Add 3 points to Team 0 if wins
+				item[0]['victory'] += 1 #Add 1 victory to Team 0 if wins
+				item[0]['total_goals'] += item[0]['goals'] #Add total goals from match
+				item[0]['goals_taken'] += item[1]['goals'] #Add total goals taken from match
 				item[0]['goal_balance'] = item[0]['total_goals'] - item[0]['goals_taken']
-				item[1]['loss'] +=1
+				item[1]['loss'] += 1
 				item[1]['total_goals'] += item[1]['goals']
 				item[1]['goals_taken'] += item[0]['goals']
 				item[1]['goal_balance'] = item[1]['total_goals'] - item[1]['goals_taken']
@@ -81,16 +81,14 @@ class App:
 				item[1]['goal_balance'] = item[1]['total_goals'] - item[1]['goals_taken']
 		with open('src/groups.txt','w') as file:
 			file.write(str(self.groups))
-		with open('src/results.txt','w') as file:
+		with open('src/group_stage_matches.txt','w') as file:
 			[file.write(str(item+'\n'))for item in results]
 
 
 
 
-	def eliminate_by_points(self):
+	def classification_for_round_of_sixteen(self):
 		this_round = []
-		
-		#[this_round.append(items) and items.sort(key=lambda i: (i['points'],i['goal_balance'],i['total_goals']), reverse=True) for items in self.groups]
 		for items in self.groups:
 			items.sort(key=lambda i: (i['points'],i['goal_balance'],i['total_goals'] ),reverse=True)
 			this_round.append(items)
@@ -104,23 +102,23 @@ class App:
 			print("#################################################################################### ")
 		
 		# aqui estou pegando os 2 primeiros de cada grupo e atribuindo para next_stage
-		self.round_oc = [item[0:2] for item in this_round]
+		self.round_of_sixteen = [item[0:2] for item in this_round]
 		
 	
-	def oc(self):
+	def round_of_sixteen_matches(self):
 
-		print(f"{self.round_oc[0][0]['team']} X {self.round_oc[1][1]['team']}")
-		print(f"{self.round_oc[0][1]['team']} X {self.round_oc[1][0]['team']}")
-		print(f"{self.round_oc[2][0]['team']} X {self.round_oc[3][1]['team']}")
-		print(f"{self.round_oc[2][1]['team']} X {self.round_oc[3][0]['team']}")
-		print(f"{self.round_oc[4][0]['team']} X {self.round_oc[5][1]['team']}")
-		print(f"{self.round_oc[4][1]['team']} X {self.round_oc[5][0]['team']}")
-		print(f"{self.round_oc[6][0]['team']} X {self.round_oc[7][1]['team']}")
-		print(f"{self.round_oc[6][1]['team']} X {self.round_oc[7][0]['team']}")
+		print(f"{self.round_of_sixteen[0][0]['team']} X {self.round_of_sixteen[1][1]['team']}")
+		print(f"{self.round_of_sixteen[0][1]['team']} X {self.round_of_sixteen[1][0]['team']}")
+		print(f"{self.round_of_sixteen[2][0]['team']} X {self.round_of_sixteen[3][1]['team']}")
+		print(f"{self.round_of_sixteen[2][1]['team']} X {self.round_of_sixteen[3][0]['team']}")
+		print(f"{self.round_of_sixteen[4][0]['team']} X {self.round_of_sixteen[5][1]['team']}")
+		print(f"{self.round_of_sixteen[4][1]['team']} X {self.round_of_sixteen[5][0]['team']}")
+		print(f"{self.round_of_sixteen[6][0]['team']} X {self.round_of_sixteen[7][1]['team']}")
+		print(f"{self.round_of_sixteen[6][1]['team']} X {self.round_of_sixteen[7][0]['team']}")
 		
 app = App()
 app.define_cup()
 app.define_groups()
-app.team_matches()
-app.eliminate_by_points()
-app.oc()
+app.team_matches_groups()
+app.classification_for_round_of_sixteen()
+app.round_of_sixteen_matches()
